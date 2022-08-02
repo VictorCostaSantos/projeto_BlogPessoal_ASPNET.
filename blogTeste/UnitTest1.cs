e2ad01c2e6e01fd69728c4458f4504cbed24ac13
VictorCostaSantos <victorcosta123vcs@gmail.com>
2022-08-02 11:51:34 -0300

    Primeiro Commit

---
+++ blogTeste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace blogTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
